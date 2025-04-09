2d1103d28b2effe80672d994a612ace8a7446397
qa-03 <qa-03@qwertysystem.net>
2025-04-09 15:13:38 +0700

    Ngumpulin test object di Item

---
+++ Object Repository/Supply-Item/Button Add Place Stock.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Button Add Place Stock</name>
+   <tag></tag>
+   <elementGuidId>431b9aae-89ab-494a-ad39-53176c5aba46</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>button[aria-label=&quot;Place Stock&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
