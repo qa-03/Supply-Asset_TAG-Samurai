278ea1b85340ae8a3f905511abb19f67e9743d4b
qa-03 <qa-03@qwertysystem.net>
2025-04-10 17:20:22 +0700

    Bikin Test Case buat Sidebar Category

---
+++ Object Repository/Sidebar Category (Can be Multimenu)/Button New Category.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Button New Category</name>
+   <tag></tag>
+   <elementGuidId>e29b8252-86b7-4488-8ab7-4df0e791e017</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[text()='New Category']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Sidebar Category (Can be Multimenu)/Field Category Name di New Category.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Field Category Name di New Category</name>
+   <tag></tag>
+   <elementGuidId>19a3ab83-dd0f-4e73-a36b-837981f33d06</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>input[placeholder=&quot;Enter category name&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
